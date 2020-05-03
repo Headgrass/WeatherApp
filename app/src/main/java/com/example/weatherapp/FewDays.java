487f0276eee87e017a4b9316130111ab312acdac
Vladimir Alekseev <headgrass@yandex.ru>
2020-05-03 18:24:17 +0700

    Сделал внешний вид

--- app/src/main/java/com/example/weatherapp/FewDays.java
+++ app/src/main/java/com/example/weatherapp/FewDays.java
@@ -1,4 +1,21 @@
 package com.example.weatherapp;
 
-public class FewDays {
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class FewDays extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+       // return super.onCreateView(inflater, container, savedInstanceState);
+        View rootView =
+                inflater.inflate(R.layout.few_days, container, false);
+        return rootView;
+    }
 }
